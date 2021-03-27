5d0cf57eaf76d4d1e3b9fca9ad80dc889dad5496
Genésio Valois <genas96@gmail.com>
2021-03-27 17:53:28 -0300

    feat: Import auth reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { loadingBarReducer } from "react-redux-loading-bar";
+import authReducer from "./authReducer";
 
 export default combineReducers({
   loadingBar: loadingBarReducer,
+  auth: authReducer,
 });
