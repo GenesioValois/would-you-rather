731c3187af2cc203d9a2ac15408f47a5f3e1c623
Genésio Valois <genas96@gmail.com>
2021-03-29 13:40:32 -0300

    refactor: Add container element

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
+import Container from "@material-ui/core/Container";
 import { handleInitialData } from "./store/actions/shared";
 import Home from "./pages/home";
 import Login from "./pages/login";
@@ -16,14 +16,14 @@ function App() {
   }, []);
 
   return (
-    <>
+    <Container fixed>
       <LoadingBar />
       <Router>
         <Switch>
           <Route path="/" exact component={isSignedIn ? Home : Login} />
         </Switch>
       </Router>
-    </>
+    </Container>
   );
 }
 
