844f530c2fab7d5d8148c204d161cdfba9b87b13
Genésio Valois <genas96@gmail.com>
2021-03-30 11:39:33 -0300

    feat: Add add_answer reducer on user reducer
    The answer id has to be updated at question state and as weel at user
    state

--- src/store/reducers/usersReducer.js
+++ src/store/reducers/usersReducer.js
@@ -1,11 +1,25 @@
 import { SET_USERS } from "../actions/user";
+import { ADD_ANSWER } from "../actions/question";
 
-export default function authedUser(state = {}, action) {
+export default function usersReducer(state = {}, action) {
   switch (action.type) {
     case SET_USERS:
       return {
         ...state,
         ...action.users,
+      };
+    case ADD_ANSWER:
+      const { question_id, answer, user_id } = action.answer;
+
+      return {
+        ...state,
+        [user_id]: {
+          ...state[user_id],
+          answers: {
+            ...state[user_id].answers,
+            [question_id]: answer,
+          },
+        },
       };
     default:
       return state;
