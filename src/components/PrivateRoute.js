53a05ebf65911980c0dc429842df1ba6822d4187
Genésio Valois <genas96@gmail.com>
2021-03-30 14:55:31 -0300

    docs: Add small comments on third parties code

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -3,11 +3,13 @@ import { useSelector } from "react-redux";
 import { Route } from "react-router-dom";
 import Login from "../pages/login";
 
-function PrivateRoute({ comp: Component, ...rest }) {
+// the implementation of this compoenent is based at:
+// https://medium.com/@tomlarge/private-routes-with-react-router-dom-28e9f40c7146
+const PrivateRoute = ({ comp: Component, ...rest }) => {
   const isSignedIn = useSelector(({ auth }) => auth.isSignedIn);
   return (
     <Route {...rest} render={(props) => (isSignedIn ? <Component {...props} /> : <Login />)} />
   );
-}
+};
 
 export default PrivateRoute;
