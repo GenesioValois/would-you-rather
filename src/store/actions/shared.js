27bb5859ed16808297f4573ef8774e737c47a5bc
Genésio Valois <genas96@gmail.com>
2021-03-30 11:37:47 -0300

    feat: Set question and user at handleInitialData

--- src/store/actions/shared.js
+++ src/store/actions/shared.js
@@ -2,11 +2,13 @@ import { showLoading, hideLoading } from "react-redux-loading-bar";
 
 import { getInitialData } from "../../api";
 import { setUsers } from "./user";
+import { setQuestions } from "./question";
 
 export const handleInitialData = () => (dispatch) => {
   dispatch(showLoading());
-  return getInitialData().then(({ users }) => {
+  return getInitialData().then(({ users, questions }) => {
     dispatch(setUsers(users));
+    dispatch(setQuestions(questions));
     dispatch(hideLoading());
   });
 };
