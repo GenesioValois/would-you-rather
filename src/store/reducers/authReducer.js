37baa99f53017a3928a3af0a4db0812dcc433aaa
Genésio Valois <genas96@gmail.com>
2021-03-30 14:25:17 -0300

    refactor: Remove initial state from localstorage

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,8 +1,6 @@
 import { SIGN_IN, SIGN_OUT } from "../actions/auth";
 
-const user_id = localStorage.getItem("user_id");
-const initialState = user_id ? { isSignedIn: true, user_id } : {};
-export default function authedUser(state = initialState, action) {
+export default function authedUser(state = {}, action) {
   switch (action.type) {
     case SIGN_IN:
       return { ...state, isSignedIn: true, user_id: action.id };
