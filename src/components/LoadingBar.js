95ef3ce0e2fd7faa00e07a53cb33955dc98aab1b
Genésio Valois <genas96@gmail.com>
2021-03-30 11:39:13 -0300

    feat: Change top value if user is logged in

--- src/components/LoadingBar.js
+++ src/components/LoadingBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import clsx from "clsx";
 import { useSelector } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
 import LinearProgress from "@material-ui/core/LinearProgress";
@@ -9,14 +10,26 @@ const useStyles = makeStyles(() => ({
     width: "100%",
     top: 0,
   },
+  topZero: { top: 0 },
+  topHeader: { top: 64 },
 }));
 
 const LoadingBar = () => {
+  const isSignedIn = useSelector(({ auth }) => auth.isSignedIn);
   const classes = useStyles();
   const loadingBar = useSelector(({ loadingBar }) => loadingBar);
 
   return (
-    <>{loadingBar && loadingBar.default === 1 && <LinearProgress className={classes.loading} />}</>
+    <>
+      {loadingBar && loadingBar.default === 1 && (
+        <LinearProgress
+          className={clsx(classes.loading, {
+            [classes.topHeader]: isSignedIn,
+            [classes.topZero]: !isSignedIn,
+          })}
+        />
+      )}
+    </>
   );
 };
 
