4909b5a10336df8336a8121727b2a2fbc76d2831
Genésio Valois <genas96@gmail.com>
2021-03-30 11:37:06 -0300

    feat: Add home page content

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import Grid from "@material-ui/core/Grid";
+import QuestionList from "../components/QuestionList";
 
-function Home() {
-  return <div>Home</div>;
-}
+const Home = () => {
+  const { answeredIds, unansweredIds } = useSelector(({ auth: { user_id }, questions, users }) => {
+    const answeredIds = Object.keys(questions)
+      .filter((id) => users[user_id].answers.hasOwnProperty(id))
+      .sort((a, b) => questions[b].timestamp - questions[a].timestamp);
+
+    const unansweredIds = Object.keys(questions)
+      .filter((id) => !users[user_id].answers.hasOwnProperty(id))
+      .sort((a, b) => questions[b].timestamp - questions[a].timestamp);
+
+    return {
+      answeredIds,
+      unansweredIds,
+    };
+  });
+
+  return (
+    <Grid container spacing={2}>
+      <QuestionList ids={unansweredIds} />
+      <QuestionList ids={answeredIds} />
+    </Grid>
+  );
+};
 
 export default Home;
